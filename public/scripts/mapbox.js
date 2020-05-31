e99239ada928c0b23304c872088c825fae55f0d6
Adam Simonini <adamsimonini@users.noreply.github.com>
2020-05-31 14:57:07 -0400

    modal test working

--- public/scripts/mapbox.js
+++
@@ -1,7 +0,0 @@
-import mapbox from '/mapbox-gl';
-
-mapboxgl.accessToken = 'pk.eyJ1IjoiYWRhbWpzaW0iLCJhIjoiY2thdmF0Ym9kMGNhbTJ6a3l5dzl3ZmdrcCJ9.YcSMrFRPTc7hCZgYkJeoSQ';
-const map = new mapboxgl.Map({
-container: 'mapbox-container',
-style: 'mapbox://styles/mapbox/streets-v9'
-});
