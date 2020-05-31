19ce621552d26752b3844cbab648b5ac37c1d365
Adam Simonini <adamsimonini@users.noreply.github.com>
2020-05-31 17:39:30 -0400

    seperated mapbox code into script file

--- app.js
+++ app.js
@@ -3,15 +3,6 @@ const app = express()
 const port = 3000
 const fs = require('fs');
 const path = require('path');
-// const mapbox = require('mapbox-gl');
-
-
-// mapboxgl.accessToken = mapbox_token;
-// const map = new mapboxgl.Map({
-// container: 'mapbox-container',
-// style: 'mapbox://styles/mapbox/streets-v9'
-// });
-    // let mapbox_token = process.env.MAPBOX_TOKEN;
 
 app.use(express.static("public"));
 app.get('/', (req, res) => {
