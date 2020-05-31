58fe5576863560ce2c12c3f1f24d7c90649d3b8b
Adam Simonini <adamsimonini@users.noreply.github.com>
2020-05-31 18:09:48 -0400

    re-apply api & got commit

--- app.js
+++ app.js
@@ -3,20 +3,44 @@ const app = express()
 const port = 3000
 const fs = require('fs');
 const path = require('path');
-// const mapbox = require('mapbox-gl');
+const got = require("got");
 
+const getCovidCases = async (req, res) => {
+    try {
+        console.log('getCovidCases');
+        const result = await got.get("https://covid19-admin.deploy.solutions/api/services/app/covidcase/getall", {
+            responseType: 'json',
+        });
+        const body = result.body;
+        res.json(body);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+}
 
-// mapboxgl.accessToken = mapbox_token;
-// const map = new mapboxgl.Map({
-// container: 'mapbox-container',
-// style: 'mapbox://styles/mapbox/streets-v9'
-// });
-    // let mapbox_token = process.env.MAPBOX_TOKEN;
+const getEnvironmentalFactors = async (req, res) => {
+    try {
+        console.log('getCovidCases');
+        const result = await got.get("https://covid19-admin.deploy.solutions/api/services/app/environmentfactor/GetAll", {
+            responseType: 'json',
+        });
+        const body = result.body;
+        res.json(body);
+    } catch (error) {
+        console.log(error);
+        res.send(error);
+    }
+}
 
 app.use(express.static("public"));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+app.get('/cases', getCovidCases);
+app.get('/env', getEnvironmentalFactors);
+
+
 
 app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
 
